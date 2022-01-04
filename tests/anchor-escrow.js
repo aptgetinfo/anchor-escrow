aeb415afdbc7b580a6245219c7c7e4bee4dc0f26
aptgetinfo <deepak.2002.02.16@gmail.com>
2022-01-04 16:17:20 +0530

    target/types modified to js

--- tests/anchor-escrow.js
+++ tests/anchor-escrow.js
@@ -1,14 +1,126 @@
 const anchor = require('@project-serum/anchor');
+const Program =require('@project-serum/anchor');
+const {AnchorEscrow} =require( '../target/types/anchor_escrow.js');
+const { PublicKey, SystemProgram, Transaction } =require( '@solana/web3.js');
+const { TOKEN_PROGRAM_ID, Token } =require( "@solana/spl-token");
+const { assert } =require( "chai");
 
 describe('anchor-escrow', () => {
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.AnchorEscrow;
+    let mintA = null;
+  let mintB = null;
+  let initializerTokenAccountA = null;
+  let initializerTokenAccountB = null;
+  let takerTokenAccountA = null;
+  let takerTokenAccountB = null;
+  let vault_account_pda = null;
+  let vault_account_bump = null;
+  let vault_authority_pda = null;
+
+  const takerAmount = 1000;
+  const initializerAmount = 500;
+
+  const escrowAccount = anchor.web3.Keypair.generate();
+  const payer = anchor.web3.Keypair.generate();
+  const mintAuthority = anchor.web3.Keypair.generate();
+  const initializerMainAccount = anchor.web3.Keypair.generate();
+  const takerMainAccount = anchor.web3.Keypair.generate();
+
     const tx = await program.rpc.initialize();
     console.log("Your transaction signature", tx);
   });
+
+
+
+   it("Initialize program state", async () => {
+    // Airdropping tokens to a payer.
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, 10000000000),
+      "confirmed"
+    );
+
+    // Fund Main Accounts
+    await provider.send(
+      (() => {
+        const tx = new Transaction();
+        tx.add(
+          SystemProgram.transfer({
+            fromPubkey: payer.publicKey,
+            toPubkey: initializerMainAccount.publicKey,
+            lamports: 1000000000,
+          }),
+          SystemProgram.transfer({
+            fromPubkey: payer.publicKey,
+            toPubkey: takerMainAccount.publicKey,
+            lamports: 1000000000,
+          })
+        );
+        return tx;
+      })(),
+      [payer]
+    );
+
+    mintA = await Token.createMint(
+      provider.connection,
+      payer,
+      mintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
+    mintB = await Token.createMint(
+      provider.connection,
+      payer,
+      mintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
+    initializerTokenAccountA = await mintA.createAccount(initializerMainAccount.publicKey);
+    takerTokenAccountA = await mintA.createAccount(takerMainAccount.publicKey);
+
+    initializerTokenAccountB = await mintB.createAccount(initializerMainAccount.publicKey);
+    takerTokenAccountB = await mintB.createAccount(takerMainAccount.publicKey);
+
+    await mintA.mintTo(
+      initializerTokenAccountA,
+      mintAuthority.publicKey,
+      [mintAuthority],
+      initializerAmount
+    );
+
+    await mintB.mintTo(
+      takerTokenAccountB,
+      mintAuthority.publicKey,
+      [mintAuthority],
+      takerAmount
+    );
+
+    let _initializerTokenAccountA = await mintA.getAccountInfo(initializerTokenAccountA);
+    let _takerTokenAccountB = await mintB.getAccountInfo(takerTokenAccountB);
+
+    assert.ok(_initializerTokenAccountA.amount.toNumber() == initializerAmount);
+    assert.ok(_takerTokenAccountB.amount.toNumber() == takerAmount);
+  });
+
+  // it("Initialize escrow", async () => {
+  //   // TODO
+  // });
+
+  // it("Exchange escrow state", async () => {
+  //   // TODO
+  // });
+
+  // it("Initialize escrow and cancel escrow", async () => {
+  //   // TODO
+  // });
 });
