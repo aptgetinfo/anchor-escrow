4bba68540508db2af4a69daac436451df2f44154
aptgetinfo <deepak.2002.02.16@gmail.com>
2022-01-05 17:50:35 +0530

    program state init done and tested

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { useState } from 'react';
-import { Connection, PublicKey } from '@solana/web3.js';
+
+
+
+import { Connection,  PublicKey, SystemProgram, Transaction, Keypair } from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID, Token } from  "@solana/spl-token";
 import { Program, Provider, web3 } from '@project-serum/anchor';
 import idl from './idl.json';
 
@@ -11,11 +15,22 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 
 const wallets = [ getPhantomWallet() ];
 
-const { SystemProgram, Keypair } = web3;
+let mintA = null;
+let mintB = null;
+let initializerTokenAccountA = null;
+let initializerTokenAccountB = null;
+let takerTokenAccountA = null;
+let takerTokenAccountB = null;
+let vault_account_pda = null;
+let vault_account_bump = null;
+let vault_authority_pda = null;
+
+const takerAmount = 1000;
+const initializerAmount = 500;
+
 const escrowAccount = Keypair.generate();
 const payer = Keypair.generate();
 const mintAuthority = Keypair.generate();
-const initializerMainAccount = Keypair.generate();
 const takerMainAccount = Keypair.generate();
 const opts = {
   preflightCommitment: "processed"
@@ -23,14 +38,12 @@ const opts = {
 const programID = new PublicKey(idl.metadata.address);
 
 function App() {
-  const [value, setValue] = useState('');
-  const [dataList, setDataList] = useState([]);
-  const [input, setInput] = useState('');
+  // const [value, setValue] = useState('');
+  // const [dataList, setDataList] = useState([]);
+  // const [input, setInput] = useState('');
   const wallet = useWallet();
 
   async function getProvider() {
-    /* create the provider and return it to the caller */
-    /* network set to local network for now */
     const network = "http://127.0.0.1:8899";
     const connection = new Connection(network, opts.preflightCommitment);
 
@@ -45,41 +58,82 @@ function App() {
     /* create the program interface combining the idl, program ID, and provider */
     const program = new Program(idl, programID, provider);
     try {
-      /* interact with the program via rpc */
-      await program.rpc.initialize("Hello World", {
-        accounts: {
-          baseAccount: baseAccount.publicKey,
-          user: provider.wallet.publicKey,
-          systemProgram: SystemProgram.programId,
-        },
-        signers: [baseAccount]
-      });
+      await provider.connection.confirmTransaction(
+        await provider.connection.requestAirdrop(payer.publicKey, 100000000000),
+        "confirmed"
+      );
+  
+      // Fund Main Accounts
+      await provider.send(
+        (() => {
+          const tx = new Transaction();
+          tx.add(
+            SystemProgram.transfer({
+              fromPubkey: payer.publicKey,
+              toPubkey:  provider.wallet.publicKey,
+              lamports: 1000000000,
+            }),
+            SystemProgram.transfer({
+              fromPubkey: payer.publicKey,
+              toPubkey: takerMainAccount.publicKey,
+              lamports: 1000000000,
+            })
+          );
+          return tx;
+        })(),
+        [payer] //sign of payer
+      );
+  
+      mintA = await Token.createMint(
+        provider.connection,   //connection
+        payer,        //signer
+        mintAuthority.publicKey,   //mint authority
+        null,          //freze authority
+        0,                //number
+        TOKEN_PROGRAM_ID   //program id
+      );
+  
+      mintB = await Token.createMint(
+        provider.connection,
+        payer,
+        mintAuthority.publicKey,
+        null,
+        0,
+        TOKEN_PROGRAM_ID
+      );
+  
+      initializerTokenAccountA = await mintA.createAccount(provider.wallet.publicKey);
+      takerTokenAccountA = await mintA.createAccount(takerMainAccount.publicKey);
+  
+      initializerTokenAccountB = await mintB.createAccount(provider.wallet.publicKey);
+      takerTokenAccountB = await mintB.createAccount(takerMainAccount.publicKey);
+  
+      await mintA.mintTo(
+        initializerTokenAccountA,  //to
+        mintAuthority.publicKey,   //authority
+        [mintAuthority],           //authority signature
+        initializerAmount         //amount
+      );
+  
+      await mintB.mintTo(
+        takerTokenAccountB,
+        mintAuthority.publicKey,
+        [mintAuthority],
+        takerAmount
+      );
+  
+      let _initializerTokenAccountA = await mintA.getAccountInfo(initializerTokenAccountA);
+      let _takerTokenAccountB = await mintB.getAccountInfo(takerTokenAccountB);
+  
 
-      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-      console.log('account: ', account);
-      setValue(account.data.toString());
-      setDataList(account.dataList);
+      console.log(_initializerTokenAccountA.amount.toNumber())
+      console.log( _takerTokenAccountB.amount.toNumber())
     } catch (err) {
       console.log("Transaction error: ", err);
     }
   }
 
-  async function update() {
-    if (!input) return;
-    const provider = await getProvider();
-    const program = new Program(idl, programID, provider);
-    await program.rpc.update(input, {
-      accounts: {
-        baseAccount: baseAccount.publicKey
-      }
-    });
 
-    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log('account: ', account);
-    setValue(account.data.toString());
-    setDataList(account.dataList);
-    setInput('');
-  }
 
   if (!wallet.connected) {
     return (
@@ -91,28 +145,8 @@ function App() {
     return (
       <div className="App">
         <div>
-          {
-            !value && (<button onClick={initialize}>Initialize</button>)
-          }
-
-          {
-            value ? (
-              <div>
-                <h2>Current value: {value}</h2>
-                <input
-                  placeholder="Add new data"
-                  onChange={e => setInput(e.target.value)}
-                  value={input}
-                />
-                <button onClick={update}>Add data</button>
-              </div>
-            ) : (
-              <h3>Please Inialize.</h3>
-            )
-          }
-          {
-            dataList.map((d, i) => <h4 key={i}>{d}</h4>)
-          }
+<button onClick={initialize}>Initialize</button>)
+         
         </div>
       </div>
     );
